27e18192bd5a76f07e0697676a4b9f32cc56cac1
element <element.zhan@gmail.com>
2019-09-11 10:37:57 +0800

    Add the authority to different user.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # vue-steps
 a Vue sample with gradually improving.
 
+通过极客时间的学习, 尝试做的一个模版Vue网站, 用来管理企业的中后台系统.
--- src/router.js
+++ src/router.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import Router from "vue-router";
 //import Home from "./views/Home.vue";
-import NotFound from "./views/404.vue";
 import NProgress from "nprogress";
+import findLast from "lodash/findLast";
 import "nprogress/nprogress.css";
+import NotFound from "./views/404";
+import Forbidden from "./views/403";
+import { isLogin, check } from "./utils/auth";
+import { notification } from "ant-design-vue";
 
 Vue.use(Router);
 
@@ -37,6 +41,7 @@ const router = new Router({
     },
     {
       path: "/",
+      meta: { authority: ["user", "admin"] },
       component: () =>
         import(/* webpackChunkName: "layout" */ "./layout/BasicLayout"),
       children: [
@@ -66,7 +71,7 @@ const router = new Router({
           path: "/form",
           name: "form",
           component: { render: h => h("router-view") },
-          meta: { icon: "form", title: "表格" },
+          meta: { icon: "form", title: "表格", authority: ["admin"] },
           children: [
             {
               path: "/form/basic-form",
@@ -122,6 +127,13 @@ const router = new Router({
       name: "404",
       hideInMenu: true,
       component: NotFound
+    },
+    {
+      path: "/403",
+      replace: true,
+      name: "403",
+      hideInMenu: true,
+      component: Forbidden
     }
     // {
     //   path: "/about",
@@ -137,6 +149,23 @@ const router = new Router({
 router.beforeEach((to, from, next) => {
   if (to.path !== from.path) {
     NProgress.start();
+  }
+  const record = findLast(to.matched, record => record.meta.authority);
+  if (record && !check(record.meta.authority)) {
+    if (!isLogin() && to.path !== "/user/login") {
+      next({
+        path: "/user/login"
+      });
+    } else if (to.path !== "/403") {
+      notification.error({
+        message: "403",
+        description: "对不起, 没有权限."
+      });
+      next({
+        path: "/403"
+      });
+    }
+    NProgress.done();
   }
   next();
 });

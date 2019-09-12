20cef0155639e924a0cc61d014258947aee01a22
element <element.zhan@gmail.com>
2019-09-12 12:25:16 +0800

    Have a try on e-charts.

---
+++ src/components/Chart.vue
@@ -0,0 +1,50 @@
+<template>
+  <div ref="chartDom" style="height:500px"></div>
+</template>
+
+<script>
+import echarts from "echarts";
+import debounce from "lodash/debounce";
+import { addListener, removeListener } from "resize-detector";
+export default {
+  props: {
+    option: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  watch: {
+    // option: {
+    //   handler(val) {
+    //     this.myChart.setOption(val);
+    //   },
+    //   deep: true
+    // },
+    option(val) {
+      this.myChart.setOption(val);
+    }
+  },
+  created() {
+    this.resize = debounce(this.resize, 300);
+  },
+  mounted() {
+    this.renderChart();
+    addListener(this.$refs.chartDom, this.resize);
+    // 绘制图表
+  },
+  beforeDestroy() {
+    removeListener(this.$refs.charDom, this.resize);
+    this.myChart.dispose();
+    this.myChart = null;
+  },
+  methods: {
+    resize() {
+      this.myChart.resize();
+    },
+    renderChart() {
+      this.myChart = echarts.init(this.$refs.chartDom);
+      this.myChart.setOption(this.option);
+    }
+  }
+};
+</script>

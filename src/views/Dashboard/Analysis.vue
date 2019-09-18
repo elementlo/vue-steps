726e344fcb4f0bbc6c7e3fa9c3935f1187e16f89
element <element.zhan@gmail.com>
2019-09-18 18:40:01 +0800

    Add local mock data server using webpack and axios.

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -5,40 +5,49 @@
 </template>
 
 <script>
-import random from "lodash/random";
 import Chart from "../../components/Chart";
+import axios from "axios";
 export default {
   data() {
     return {
-      chartOption: {
-        title: {
-          text: "ECharts 入门示例"
-        },
-        tooltip: {},
-        xAxis: {
-          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-        },
-        yAxis: {},
-        series: [
-          {
-            name: "销量",
-            type: "bar",
-            data: [5, 20, 36, 10, 10, 20]
-          }
-        ]
-      }
+      chartOption: {}
     };
   },
   components: {
     Chart
   },
   mounted() {
+    this.getChartData();
     this.interval = setInterval(() => {
-      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
-        () => random(100)
-      );
-      this.chartOption = { ...this.chartOption };
+      this.getChartData();
     }, 3000);
+  },
+  methods: {
+    getChartData() {
+      axios
+        .get("/api/dashboard/chart", {
+          params: { ID: 12345 }
+        })
+        .then(response => {
+          this.chartOption = {
+            title: {
+              text: "爱斐斐"
+            },
+            tooltip: {},
+            xAxis: {
+              data: ["亲亲", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [
+              {
+                name: "销量",
+                type: "bar",
+                data: response.data
+              }
+            ]
+          };
+        });
+    }
   },
   beforeDestroy() {
     clearInterval(this.interval);
